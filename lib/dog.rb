14a320694ab373cac4b481e9c11c9c4079116099
Daniel Etter <etter.dt@gmail.com>
2018-04-18 16:04:13 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+class Dog 
+  def initialize(bark)
+    @bark=bark
+  end
+  
+  def bark=bark
+    @bark=bark
+  end
+  
+  def bark
+    @bark
+    puts "woof!"
+  end
+  
+  def name=name
+    @name=name
+  end
+  
+  def name
+    @name
+  end
+end
